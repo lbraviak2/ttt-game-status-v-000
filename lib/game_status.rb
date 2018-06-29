a7f793639586b97cb7def36a03b95098770ec8cb
Lindsay Braviak <lbraviak@gmail.com>
2018-06-29 16:43:53 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,4 +7,7 @@ WIN_COMBINATIONS = [
   [0,1,2], [3,4,5],[6,7,8],[0,4,8], [2,4,6], [0,3,6], [1,4,7], [2,5,8]
 ]
 
+def won?(board, WIN_COMBINATIONS)
+  board.any?{|item| item != "" || item !=" "}        
+end
 # Define your WIN_COMBINATIONS constant
